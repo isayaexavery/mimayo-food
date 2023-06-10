c2d3e3d05e3c8db2235fd956ac5c0ad5cabe526d
isayaexavery <isayaexavery@yahoo.com>
2023-06-10 20:12:36 +0300

    edited final

--- components/HeadLinesCards.jsx
+++ components/HeadLinesCards.jsx
@@ -3,71 +3,55 @@ import React from "react";
 
 const HeadLinesCards = () => {
   return (
-    <div className="max-w-[1640px] mx-auto py-12 grid md:grid-cols-3 gap-6">
-      {/* card */}
+    <div className="max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6">
+      {/* Card */}
       <div className="rounded-xl relative">
-        <div className="absolute w-full h-full p-4 bg-black/50 rounded-xl text-white">
-          <p
-            className="font-bold text-2xl px2
-           "
-          >
-            Sun`s Out , BOGO`s out
-          </p>
-          <p className="pb-2">Through 8/26</p>
-          <button className="border-white bg-white text-black my-2 bottom-4">
+        {/* Overlay */}
+        <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
+          <p className="font-bold text-2xl px-2 pt-4">Sun's Out, BOGO's Out</p>
+          <p className="px-2">Through 8/26</p>
+          <button className="border-white bg-white text-black mx-2 absolute bottom-4">
             Order Now
           </button>
         </div>
-        <Image
+        <img
           className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
-          src="/food.jpg"
-          alt="head"
-          width={400}
-          height={200}
+          src="https://images.unsplash.com/photo-1613769049987-b31b641f25b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjF8fGJyZWFrZmFzdHxlbnwwfDB8MHx8&auto=format&fit=crop&w=800&q=60"
+          alt="/"
         />
       </div>
-      {/* card */}
+      {/* Card */}
       <div className="rounded-xl relative">
+        {/* Overlay */}
         <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
-          <p
-            className="font-bold text-2xl px2
-           p-4"
-          >
-            Sun`s Out , BOGO`s out
-          </p>
-          <p className="px-2">Through 8/26</p>
-          <button className="border-white bg-white text-black mx-2 bottom-4">
+          <p className="font-bold text-2xl px-2 pt-4">New Restaurants</p>
+          <p className="px-2">Added Daily</p>
+          <button className="border-white bg-white text-black mx-2 absolute bottom-4">
             Order Now
           </button>
         </div>
-        <Image
+        <img
           className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
-          src="/food.jpg"
-          width={400}
-          alt="head"
-          height={200}
+          src="https://images.unsplash.com/photo-1544025162-d76694265947?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGJicXxlbnwwfDB8MHx8&auto=format&fit=crop&w=800&q=60"
+          alt="/"
         />
       </div>
-      {/* card */}
+      {/* Card */}
       <div className="rounded-xl relative">
+        {/* Overlay */}
         <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
-          <p
-            className="font-bold text-2xl px2
-           pt-4"
-          >
-            Sun`s Out , BOGO`s out
+          <p className="font-bold text-2xl px-2 pt-4">
+            We Deliver Desserts Too
           </p>
-          <p className="px-2">Through 8/26</p>
-          <button className="border-white bg-white text-black mx-2 bottom-4">
+          <p className="px-2">Tasty Treats</p>
+          <button className="border-white bg-white text-black mx-2 absolute bottom-4">
             Order Now
           </button>
         </div>
-        <Image
+        <img
           className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
-          src="/food.jpg"
-          alt="head"
-          width={400}
-          height={200}
+          src="https://images.unsplash.com/photo-1559715745-e1b33a271c8f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGRlc3NlcnR8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+          alt="/"
         />
       </div>
     </div>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,90 +1,95 @@
 "use client";
 import React, { useState } from "react";
-import SearchInput from "./SearchInput";
-import { AiOutlineClose, AiOutlineMenu, AiOutlineSearch } from "react-icons/ai";
-import { BsFillCartFill } from "react-icons/bs";
+import {
+  AiOutlineMenu,
+  AiOutlineSearch,
+  AiOutlineClose,
+  AiFillTag,
+} from "react-icons/ai";
+import { BsFillCartFill, BsFillSaveFill } from "react-icons/bs";
 import { TbTruckDelivery } from "react-icons/tb";
 import { FaUserFriends, FaWallet } from "react-icons/fa";
+import { MdFavorite, MdHelp } from "react-icons/md";
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
+
   return (
     <div className="max-w-[1640px] mx-auto flex justify-between items-center p-4">
-      {/* left side */}
-      <div className="flex  items-center">
-        <div
-          onClick={() => {
-            setNav(true);
-          }}
-          className="cursor-pointer"
-        >
+      {/* Left side */}
+      <div className="flex items-center">
+        <div onClick={() => setNav(!nav)} className="cursor-pointer">
           <AiOutlineMenu size={30} />
         </div>
-        <h1 className="text 2xl sm:text-3xl lg:text-4xl px-2">
-          Mimayo <span>Eats</span>
+        <h1 className="text-2xl sm:text-3xl lg:text-4xl px-2">
+          Mimayo <span className="font-bold">Foods</span>
         </h1>
         <div className="hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]">
           <p className="bg-black text-white rounded-full p-2">Delivery</p>
           <p className="p-2">Pickup</p>
         </div>
       </div>
-      {/* search input */}
-      <div className="bg-gray-200 rounded-full flex items-center px-2 w-[200px] sm:w-[400] lg:w-[500px]">
-        <AiOutlineSearch size={20} />
+
+      {/* Search Input */}
+      <div className="bg-gray-200 rounded-full flex items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px]">
+        <AiOutlineSearch size={25} />
         <input
           className="bg-transparent p-2 w-full focus:outline-none"
           type="text"
-          placeholder="Search food"
+          placeholder="Search foods"
         />
       </div>
+      {/* Cart button */}
       <button className="bg-black text-white hidden md:flex items-center py-2 rounded-full">
-        <BsFillCartFill className="mr-2" size={20} /> Cart
+        <BsFillCartFill size={20} className="mr-2" /> Cart
       </button>
-      {/* mobile menu */}
-      {/* overlay */}
+
+      {/* Mobile Menu */}
+      {/* Overlay */}
       {nav ? (
         <div className="bg-black/80 fixed w-full h-screen z-10 top-0 left-0"></div>
       ) : (
         ""
       )}
 
-      {/* side drawer */}
+      {/* Side drawer menu */}
       <div
         className={
           nav
-            ? "fixed top-10 left-0 w-[300px] h-screen bg-white z-10 duration-300"
-            : "fixed top-10 left-[-100%] w-[300px] h-screen bg-white z-10 duration-300"
+            ? "fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300"
+            : "fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-300"
         }
       >
         <AiOutlineClose
-          onClick={() => setNav(false)}
+          onClick={() => setNav(!nav)}
           size={30}
           className="absolute right-4 top-4 cursor-pointer"
         />
         <h2 className="text-2xl p-4">
-          Best <span>Eats</span>
+          Best <span className="font-bold">Eats</span>
         </h2>
         <nav>
           <ul className="flex flex-col p-4 text-gray-800">
             <li className="text-xl py-4 flex">
-              <TbTruckDelivery size={25} className="mr-4" />
-              Orders
+              <TbTruckDelivery size={25} className="mr-4" /> Orders
             </li>
             <li className="text-xl py-4 flex">
-              <TbTruckDelivery size={25} className="mr-4" />
-              Help
+              <MdFavorite size={25} className="mr-4" /> Favorites
             </li>
             <li className="text-xl py-4 flex">
-              <FaWallet size={25} className="mr-4" />
-              Wallet
+              <FaWallet size={25} className="mr-4" /> Wallet
             </li>
             <li className="text-xl py-4 flex">
-              <FaUserFriends size={25} className="mr-4" />
-              Friends
+              <MdHelp size={25} className="mr-4" /> Help
             </li>
             <li className="text-xl py-4 flex">
-              <FaUserFriends size={25} className="mr-4" />
-              Invite
+              <AiFillTag size={25} className="mr-4" /> Promotions
+            </li>
+            <li className="text-xl py-4 flex">
+              <BsFillSaveFill size={25} className="mr-4" /> Best Ones
+            </li>
+            <li className="text-xl py-4 flex">
+              <FaUserFriends size={25} className="mr-4" /> Invite Friends
             </li>
           </ul>
         </nav>
